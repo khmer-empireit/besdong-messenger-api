786a7c0e4a7be9fe099447eb04f73a0c70aef594
sokheng-sreng <sokheng-sreng@khmer-empire.com>
2026-05-11 11:25:16 +0700

    add HTTP request logging middleware

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import 'reflect-metadata';
 import { NestFactory } from '@nestjs/core';
-import { ValidationPipe, VersioningType } from '@nestjs/common';
+import { ValidationPipe, VersioningType, Logger } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import * as path from 'path';
@@ -10,7 +10,15 @@ import { ResponseInterceptor } from './shared/interceptors/response.interceptor'
 import helmet from 'helmet';
 
 async function bootstrap() {
+  const logger = new Logger('HTTP');
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.use((req: any, res: any, next: any) => {
+    res.on('finish', () => {
+      logger.log(`${req.method} ${req.originalUrl} ${res.statusCode}`);
+    });
+    next();
+  });
 
   // Serve local uploads as static files at /uploads/*
   app.useStaticAssets(path.resolve('./uploads'), { prefix: '/uploads' });
