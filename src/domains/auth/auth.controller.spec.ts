0961cdb8352c613e50ad51259a27fa9253996fa0
sokheng-sreng <sokheng-sreng@khmer-empire.com>
2026-05-11 10:01:08 +0700

    fix supertest namespace import in auth controller spec

--- src/domains/auth/auth.controller.spec.ts
+++ src/domains/auth/auth.controller.spec.ts
@@ -1,6 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication, ValidationPipe, VersioningType } from '@nestjs/common';
-import * as request from 'supertest';
+import request from 'supertest';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { JwtService } from '@nestjs/jwt';
